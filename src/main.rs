15843c9a5e9fab0b9930c7cb5927ca47db26fcde
Guy Marshall <guymarshall9@gmail.com>
2022-11-10 21:36:00 +0000

    Add extra divide by 2 to speed up collatz

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn collatz(mut number: i128) -> i128 {
         if number % 2 == 0 {
             number = number / 2;
         } else {
-            number = (3 * number) + 1;
+            number = ((3 * number) + 1) / 2;
         }
     }
 
