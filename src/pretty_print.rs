41e0fcef631793b8fa56e75195f9e6753202a499
Guy Marshall <guymarshall9@gmail.com>
2022-12-21 11:33:36 +0000

    Add num to cargo.toml + commented bigint code to use to convert to bigint

--- src/pretty_print.rs
+++ src/pretty_print.rs
@@ -1,8 +1,38 @@
 #![forbid(unsafe_code)]
 
+// extern crate num;
+// use num::BigInt;
+
+/**
+ * ERRORS OCCUR CONVERTING BIGINT TO EXPONENTIAL NOTATION - NEED TO MAKE MY OWN FUNCTION FOR THIS
+ */
+
 pub fn format_result(number: i128) -> String {
     if format!("{}", number).chars().count() < 5 {
         return format!("{}", number);
     }
     format!("{:.5e}", number)
 }
+
+/*
+extern crate num;
+use num::BigInt;
+
+fn number_to_vector(number: i32) -> Vec<BigInt> {
+    let mut numbers: Vec<BigInt> = Vec::new();
+
+    (1..=number).into_iter().for_each(|i| {
+        numbers.push(BigInt::from(i));
+    });
+
+    numbers
+}
+
+pub fn factorial(number: i32) -> BigInt {
+    let numbers: Vec<BigInt> = number_to_vector(number);
+
+    let factorial: BigInt = numbers.iter().fold(BigInt::from(1), |acc, x| acc * x);
+
+    factorial
+}
+*/
