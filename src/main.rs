49f6f524e89ee00d6e4b4e8eeefc63b4136b8f51
Guy Marshall <guymarshall9@gmail.com>
2022-12-21 11:25:47 +0000

    Use format_result to print large numbers as exponents

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,22 @@
 
 mod user_input;
 mod collatz;
+mod pretty_print;
 
 fn main() {
     let user_input: i128 = user_input::get_user_input("Enter a positive integer: ");
 
     let mut max_number: i128 = 0;
     let mut max_steps: i128 = 0;
+    let mut result: i128 = 0;
 
     for i in 1..=user_input {
-        let result: i128 = collatz::collatz(i);
+        result = collatz::collatz(i);
         if max_steps < result {
             max_steps = result;
             max_number = i;
         }
     }
 
-    println!("{} had the maximum number of steps at {}", max_number, max_steps);
+    println!("{} had {} steps", pretty_print::format_result(user_input), pretty_print::format_result(result));
 }
