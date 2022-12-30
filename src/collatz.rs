393ef568ca2e03e627e8803a43cd78662952eabd
Guy Marshall <guymarshall9@gmail.com>
2022-12-30 22:40:37 +0000

    Rename dereferenced_number to number_clone

--- src/collatz.rs
+++ src/collatz.rs
@@ -5,16 +5,16 @@ use num::BigInt;
 
 pub fn collatz(number: &BigInt) -> BigInt {
     let mut counter: BigInt = BigInt::from(0);
-    let mut dereferenced_number: BigInt = number.clone();
+    let mut number_clone: BigInt = number.clone();
 
     //if even, divide by 2
     //else times by 3 and add 1
-    while &dereferenced_number > &BigInt::from(1) {
+    while &number_clone > &BigInt::from(1) {
         counter += 1;
-        if &dereferenced_number % BigInt::from(2) == BigInt::from(0) {
-            dereferenced_number = dereferenced_number / 2;
+        if &number_clone % BigInt::from(2) == BigInt::from(0) {
+            number_clone = number_clone / 2;
         } else {
-            dereferenced_number = ((3 * dereferenced_number) + 1) / 2;
+            number_clone = ((3 * number_clone) + 1) / 2;
         }
     }
 
