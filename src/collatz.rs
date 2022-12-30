878b15590ed1c5bcb91ea3d12e370210172da90a
Guy Marshall <guymarshall9@gmail.com>
2022-12-30 22:38:50 +0000

    Move clone from main() to collatz()

--- src/collatz.rs
+++ src/collatz.rs
@@ -3,9 +3,9 @@
 extern crate num;
 use num::BigInt;
 
-pub fn collatz(number: BigInt) -> BigInt {
+pub fn collatz(number: &BigInt) -> BigInt {
     let mut counter: BigInt = BigInt::from(0);
-    let mut dereferenced_number: BigInt = number;
+    let mut dereferenced_number: BigInt = number.clone();
 
     //if even, divide by 2
     //else times by 3 and add 1
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,9 @@ use num::BigInt;
 
 fn main() {
     let user_input: BigInt = user_input::get_user_input("Enter a positive integer: ");
-    let printable_user_input: BigInt = user_input.clone();
 
-    let result: BigInt = collatz::collatz(user_input);
+    let result: BigInt = collatz::collatz(&user_input);
     
-    println!("{} had {} steps", printable_user_input, result);
+    println!("{} had {} steps", &user_input, result);
     // println!("{} had {} steps", pretty_print::format_result(user_input), pretty_print::format_result(result));
 }
