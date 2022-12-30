f66714deb0668ee7a9c8b78c63ad2129d7dddb39
Guy Marshall <guymarshall9@gmail.com>
2022-12-30 22:56:44 +0000

    Change get_user_input to u32 + add exponential calculation

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,13 @@ use num::BigInt;
 use crate::collatz::collatz;
 
 fn main() {
-    let user_input: BigInt = user_input::get_user_input("Enter a positive integer: ");
+    let user_input: u32 = user_input::get_user_input("Enter a positive integer: ");
 
-    let result: BigInt = collatz(&user_input);
+    let user_input_exponential: BigInt = BigInt::from(2).pow(user_input) + 1;
+    println!("Finished calculating exponential. Calculating collatz...");
+
+    let result: BigInt = collatz(&user_input_exponential);
     
-    println!("{} had {} steps", &user_input, result);
+    println!("{} had {} steps", &user_input_exponential, result);
     // println!("{} had {} steps", pretty_print::format_result(user_input), pretty_print::format_result(result));
 }
--- src/user_input.rs
+++ src/user_input.rs
@@ -1,11 +1,8 @@
 #![forbid(unsafe_code)]
 
-extern crate num;
-use num::BigInt;
-
 use std::io;
 
-pub fn get_user_input(prompt: &str) -> BigInt {
+pub fn get_user_input(prompt: &str) -> u32 {
     println!("{}", prompt);
 
     let mut user_input: String = String::new();
