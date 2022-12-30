4ae2af5de2535437e4d26ae1fa1c76fa678dea67
Guy Marshall <guymarshall9@gmail.com>
2022-12-30 23:02:38 +0000

    Improve println

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,6 @@ fn main() {
 
     let result: BigInt = collatz(&user_input_exponential);
     
-    println!("{} had {} steps", &user_input_exponential, result);
+    println!("Base: {}, Exponent: {}, Steps: {}", &base, &exponent, result);
     // println!("{} had {} steps", pretty_print::format_result(user_input), pretty_print::format_result(result));
 }
