f8c6bb4f96bc66a85000e72cec73903c1bee51e0
Guy Marshall <guymarshall9@gmail.com>
2022-12-30 22:28:42 +0000

    Fix program to run without error

--- src/collatz.rs
+++ src/collatz.rs
@@ -3,15 +3,15 @@
 extern crate num;
 use num::BigInt;
 
-pub fn collatz(number: & BigInt) -> BigInt {
+pub fn collatz(number: BigInt) -> BigInt {
     let mut counter: BigInt = BigInt::from(0);
-    let mut dereferenced_number: BigInt = *number; //needs fixing
+    let mut dereferenced_number: BigInt = number; //needs fixing
 
     //if even, divide by 2
     //else times by 3 and add 1
-    while dereferenced_number > BigInt::from(1) {
+    while &dereferenced_number > &BigInt::from(1) {
         counter += 1;
-        if dereferenced_number % BigInt::from(2) == BigInt::from(0) {
+        if &dereferenced_number % BigInt::from(2) == BigInt::from(0) {
             dereferenced_number = dereferenced_number / 2;
         } else {
             dereferenced_number = ((3 * dereferenced_number) + 1) / 2;
