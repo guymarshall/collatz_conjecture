087da61f1a97a3f160c8153a4fbd29bfdffd06e2
Guy Marshall <guymarshall9@gmail.com>
2023-04-21 18:56:58 +0100

    Add new user_input.rs

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use num::BigInt;
 use crate::collatz::collatz;
 
 fn main() {
-    let exponent: u32 = user_input::get_user_input("Enter a positive integer: ");
+    let exponent: u32 = user_input::input("Enter a positive integer: ");
 
     let base: BigInt = BigInt::from(2);
     let user_input_exponential: BigInt = base.pow(exponent) + 1;
