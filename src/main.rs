b1db22472db1edba55b3ab835b856def204ff053
Guy Marshall <guymarshall9@gmail.com>
2023-06-04 10:59:32 +0100

    Swap main.rs to use single number for checking not exponents

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,9 @@ use num::BigInt;
 use crate::collatz::collatz;
 
 fn main() {
-    let exponent: u32 = user_input::input("Enter a positive integer: ");
+    let exponent: BigInt = user_input::input("Enter a positive integer: ");
 
-    let base: BigInt = BigInt::from(2);
-    let user_input_exponential: BigInt = base.pow(exponent) + 1;
+    let result: BigInt = collatz(&exponent);
 
-    let result: BigInt = collatz(&user_input_exponential);
-
-    println!("Base: {}, Exponent: {}, Steps: {}", &base, &exponent, result);
+    println!("Number: {}, Steps: {}", &exponent, result);
 }
